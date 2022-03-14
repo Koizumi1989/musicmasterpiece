9f99c45cb3a751aeec3e515ac42969fa923b687b
Koizumi1989 <08033431635a@gmail.com>
2022-03-14 09:36:45 +0000

    footer_fix,contact_mail_complete

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,25 +1,24 @@
 class ContactsController < ApplicationController
-  
+
   def new
     @contact = Contact.new
   end
 
+  # 作成したメールオブジェクト(contact_mail)のdeliverメソッドを呼び出すことで、メールの送信が行えます。
+  # deliver_laterでコントローラは送信完了を待たずに処理を続行できます.
   def create
     @contact = Contact.new(contact_params)
     if @contact.save
-      ContactMailer.contact_mail(@contact).deliver
-      redirect_to thanks_contacts_path
+      ContactMailer.contact_mail(@contact).deliver_later
+      redirect_to root_path
     else
       render :new
     end
   end
 
-  def thanks
-  end
-
   private
 
   def contact_params
-    params.require(:contact).permit(:name, :email, :content)
+    params.require(:contact).permit(:name, :email,:phone_number, :content)
   end
 end
