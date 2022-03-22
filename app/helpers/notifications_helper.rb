1cc83101f8a648db2f3b05053d04e0e14f615d36
Koizumi1989 <08033431635a@gmail.com>
2022-03-22 14:05:56 +0000

    notification in the middle

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,5 +1,7 @@
 module NotificationsHelper
   def unchecked_notifications
-    @notifications = current_user.passive_notifications.where(checked: false)
+    if user_signed_in?
+      @notifications = current_user.passive_notifications.where(checked: false)
+    end
   end
 end
